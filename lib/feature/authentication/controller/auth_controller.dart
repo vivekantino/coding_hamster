fd8ef568ea72d12b0e498c5b0ea8b3092bff867e
vivekantino <vivek.kumar@antino.com>
2023-09-16 14:51:25 +0530

    sign up done

--- lib/feature/authentication/controller/auth_controller.dart
+++ lib/feature/authentication/controller/auth_controller.dart
@@ -1,9 +1,19 @@
 import 'package:coding_hamster/api_client/api_exception.dart';
 import 'package:coding_hamster/core/utils/utils.dart';
 import 'package:coding_hamster/feature/authentication/repository/auth_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
+  final RxBool isSignUpFilled = false.obs;
+
+  final TextEditingController firstName = TextEditingController();
+
+  final TextEditingController lastName = TextEditingController();
+
+  final TextEditingController email = TextEditingController();
+
+  final TextEditingController phoneNo = TextEditingController();
   Future<void> login() async {
     try {
       final response = await AuthRepository.login();
@@ -11,6 +21,17 @@ class AuthController extends GetxController {
       Utils.handleException(e.errorMessage.toString());
     } catch (e) {
       Utils.handleCatchException();
+    }
+  }
+
+  void checkSignUpStatus() {
+    if (firstName.text.isNotEmpty &&
+        lastName.text.isNotEmpty &&
+        email.text.isNotEmpty &&
+        phoneNo.text.isNotEmpty) {
+      isSignUpFilled.value = true;
+    } else {
+      isSignUpFilled.value = false;
     }
   }
 }
--- lib/feature/authentication/view/signup_screen.dart
+++ lib/feature/authentication/view/signup_screen.dart
@@ -1,5 +1,7 @@
+import 'package:coding_hamster/core/common_widgets/app_buttons.dart';
 import 'package:coding_hamster/core/common_widgets/app_text_field.dart';
 import 'package:coding_hamster/extension/size_extension.dart';
+import 'package:coding_hamster/feature/authentication/controller/auth_controller.dart';
 import 'package:coding_hamster/theme/app_colors.dart';
 import 'package:coding_hamster/theme/app_text_style.dart';
 import 'package:flutter/material.dart';
@@ -7,9 +9,7 @@ import 'package:get/get.dart';
 
 class SignUpScreen extends StatelessWidget {
   SignUpScreen({super.key});
-  final TextEditingController firstName = TextEditingController();
-
-  final TextEditingController lastName = TextEditingController();
+  final AuthController controller = Get.put(AuthController());
 
   @override
   Widget build(BuildContext context) {
@@ -30,59 +30,93 @@ class SignUpScreen extends StatelessWidget {
       body: Container(
         width: double.maxFinite,
         padding: EdgeInsets.symmetric(horizontal: 16.widthMultiplier),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 60.heightMultiplier,
-            ),
-            Text(
-              "Personal Detail",
-              style: AppTextStyle.f18w600Blue,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 8.heightMultiplier,
-            ),
-            Text(
-              "Please provide your details below",
-              style: AppTextStyle.f14w400Blue,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 24.heightMultiplier,
-            ),
-            AppTextField(
-              hintText: "Enter your first name",
-              labelText: "First name",
-              controller: firstName,
-            ),
-            SizedBox(
-              height: 24.heightMultiplier,
-            ),
-            AppTextField(
-              hintText: "Enter your last name",
-              labelText: "Last name",
-              controller: lastName,
-            ),
-            SizedBox(
-              height: 24.heightMultiplier,
-            ),
-            AppTextField(
-              hintText: "Phone number (Optional)",
-              labelText: "Last name",
-              controller: lastName,
-              prefix: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "+91",
-                    style: AppTextStyle.f14w400Neutral6,
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 60.heightMultiplier,
               ),
-            ),
-          ],
+              Text(
+                "Personal Detail",
+                style: AppTextStyle.f18w600Blue,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 8.heightMultiplier,
+              ),
+              Text(
+                "Please provide your details below",
+                style: AppTextStyle.f14w400Blue,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 24.heightMultiplier,
+              ),
+              AppTextField(
+                hintText: "Enter your first name",
+                labelText: "First name",
+                controller: controller.firstName,
+                onChanged: (value) {
+                  controller.checkSignUpStatus();
+                },
+              ),
+              SizedBox(
+                height: 24.heightMultiplier,
+              ),
+              AppTextField(
+                hintText: "Enter your last name",
+                labelText: "Last name",
+                controller: controller.lastName,
+                onChanged: (value) {
+                  controller.checkSignUpStatus();
+                },
+              ),
+              SizedBox(
+                height: 24.heightMultiplier,
+              ),
+              AppTextField(
+                hintText: "Enter your email",
+                labelText: "Email",
+                controller: controller.email,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  controller.checkSignUpStatus();
+                },
+              ),
+              SizedBox(
+                height: 24.heightMultiplier,
+              ),
+              AppTextField(
+                hintText: "Phone number (Optional)",
+                labelText: "Phone number",
+                controller: controller.phoneNo,
+                keyboardType: TextInputType.phone,
+                onChanged: (value) {
+                  controller.checkSignUpStatus();
+                },
+                prefix: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "+91",
+                      style: AppTextStyle.f14w400Neutral6,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 24.heightMultiplier,
+              ),
+              Obx(
+                () => AppButton.primaryMedium(
+                  onPressed:
+                      (controller.isSignUpFilled.value == true) ? () {} : null,
+                  child: const Text("Sign Up"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
